07bc0dfed424c48e02601b1c0cd39b84e63952dc
Yashika Tanwar <yashika.tanwar567@gmail.com>
2024-02-21 23:25:24 +0530

    JobListing

--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,14 @@ const router=express.Router();
 const User=require('../models/user');
 
 router.post("/register",(req,res)=>{
+    try{
+        const {name,email,password}=req.body;
+        console.log(name,email,password);
+    }
+    catch(error){
+        console.log(SyntaxError);
+    }
     console.log("Inside register");
 });
+
+module.exports=router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const express=require('express');
 const { default: mongoose } = require('mongoose');
+const auth=require('./routes/auth')
 const app=express();
 
 const host=process.env.HOST || "localhost";
 const port=process.env.PORT || 3000;
+
+//app.use() tells the server that these are the routes
+app.use('/api/v1/auth',auth)
+
 
 // connecting database to express server
 mongoose.connect(process.env.MONGODB_URI)
@@ -24,4 +29,4 @@ app.get("/health",(req,res)=>{
 
 app.listen(port,()=>{
     console.log(`Backend server running at http://${host}:${port}`)
-})
+});
