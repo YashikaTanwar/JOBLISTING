f6f924a8d31e4ec45d51a3863c27a5abc7cb00d5
Yashika Tanwar <yashika.tanwar567@gmail.com>
2024-02-24 16:11:45 +0530

    JobListing

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,39 @@
 const express=require('express');
 const router=express.Router();
 const User=require('../models/user');
+const bcrypt=require('bcrypt');
 
-router.post("/register",(req,res)=>{
+router.post("/register",async(req,res)=>{
     try{
         const {name,email,password}=req.body;
         
         // checking values if empty
         if(!name|| !email || !password)
         {
-            return res.json({
+            return res.status(400).json({
                 errorMessage:"Bad request"
             })
         }
-        console.log(name,email,password);
+        // checking if user already exists -> if already exists then will print already existing
+        const isExistingUser = await User.findOne({email:email});
+        if(isExistingUser)
+        {
+            return res.status(409).json({errorMessage:"User already exists"})
+        };
+
+        // for password we have to fetch data from client side so we use await  
+        const hasedpassword = await bcrypt.hash(password,10);
+        const userData=new User({
+            name,
+            email,
+            password : hashedpassword
+        });
+
+        // saving data in database
+        await userData.save();
+
     }
+
     catch(error){
         console.log(SyntaxError);
     }
