e7afcbecd98cb71774714d0edd3d294b25676270
Yashika Tanwar <yashika.tanwar567@gmail.com>
2024-02-26 21:50:13 +0530

    JobListing

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,13 @@ const express=require('express');
 const router=express.Router();
 const User=require('../models/user');
 const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
+router.use(express.json());
 
 router.post("/register",async(req,res)=>{
     try{
         const { name, email, password}=req.body;
-        console.log(name,password,email);
+        console.log(name,email,password);
         // checking values if empty
         if(!name|| !email || !password)
         {
@@ -22,7 +24,7 @@ router.post("/register",async(req,res)=>{
         };
 
         // for password we have to fetch data from client side so we use await  
-        const hasedpassword = await bcrypt.hash(password,10);
+        const hashedpassword = await bcrypt.hash(password,10);
         const userData=new User({
             name,
             email,
@@ -31,13 +33,46 @@ router.post("/register",async(req,res)=>{
 
         // saving data in database
         await userData.save();
-
+        res.json({message:"user registered successfully"});
     }
 
     catch(error){
-        console.log(SyntaxError);
+        console.log(error);
     }
     console.log("Inside register");
 });
 
+
+router.post("/login",async(req,res)=>{
+    try{
+        const{email,password}=req.body;
+        if(!email || !password){
+            return res.status(400).json({
+                errorMessage:"Bad request"
+            })
+        }
+
+        // if particular email exists 
+        const userDetails=await User.findOne({email});
+        // !userDetails --> this means user does not exists
+        if(!userDetails){
+            return res.status(401).json({errorMessage:"Invalid Credentials"});
+        }
+
+        // if user exists then we'll compare password
+        const passwordMatched = await bcrypt.compare(password,userDetails.password);
+        
+        // if password does not matches
+        if(!passwordMatched){
+            return res.status(401).json({errorMessage:"Invalid User Credentials"});
+        }
+
+        const token=jwt.sign({},process.env.SECRET_KEY);
+        // if everthing works fine then this message will get printed
+        res.json({message:"User Logged In",name:userDetails.name});
+    }
+    catch(error){
+        console.log(error);
+    }
+});
 module.exports=router;
