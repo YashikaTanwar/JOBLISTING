050245f96e060e329c94ab69d2a6a536053b2f34
Yashika Tanwar <yashika.tanwar567@gmail.com>
2024-02-28 15:45:27 +0530

    JobListing

--- routes/job.js
+++ routes/job.js
@@ -2,11 +2,34 @@ const express=require('express');
 const router=express.router();
 const Job=require('../models/job');
 
-router.post('/create',(req,res)=>{
+router.post('/create',async(req,res)=>{
     try{
+        const{
+            CompanyName,
+            Title,
+            Description,
+            LogoUrl,
+            Salary,
+            Location,
+            Duration,
+            LocationType
+        }=req.body;
+        console.log(CompanyName,Title,Description,LogoUrl,Salary,Location,Duration,LocationType);
+        
+        // checking if values are empty
+        if(!CompanyName || !Title || !Description || !LogoUrl || !Salary || !Location || !Duration || !LocationType){
+            return res.status(400).json
+            ({errorMessage:"Bad request"});
+        }
 
+        // checking if Job already exists in Db
+        const isExtistingJob=await Job.findOne({Title:Title, CompanyName:CompanyName, Description:Description, Salary:Salary, Location:Location})
+            if(isExtistingJob)
+            {
+                return res.status(409).json({errorMessage:"Job already Exists in DataBase"});
+            }
     }
     catch(error){
-
+        console.log(error);
     }
 });
