81b40359da2d91485bcbc6a6781ee61a95588433
Yashika Tanwar <yashika.tanwar567@gmail.com>
2024-03-04 21:39:06 +0530

    JobListing

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,17 @@
+// used for verifying tokens
 const jwt=require('jsonwebtoken');
 const verifyToken=(req,res,next)=>{
     try{
+        // authorization is a value which is defined under 'Headers' and from there we are fetching token value   
         const token=req.header("Authorization");
+
         // token <- userID
+        // this block checks if token exists if not then an error will be generated
         if(!token)
         {
             res.status(401).json({message:"Unauthorized access"});
         }
+        // for token verification
         const decode=jwt.verify(token,process.env.SECRET_KEY);
         next();
     }
