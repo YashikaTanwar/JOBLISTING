916af27439dcb86c6c78bc6ebc8f8c543ef104d7
Yashika Tanwar <yashika.tanwar567@gmail.com>
2024-04-01 23:46:50 +0530

    JobListing

--- routes/job.js
+++ routes/job.js
@@ -4,6 +4,8 @@ const router=express.Router();
 const Job=require('../models/job');
 const verifyToken=require('../middleware/authMiddleware');
 
+
+// post request is used to create / update resource.
 // verifyToken is a middleware which we have created and here it is used for authentication
 // allows to create job post in DB
 router.post('/create',verifyToken,async(req,res,next)=>{
@@ -62,6 +64,7 @@ router.post('/create',verifyToken,async(req,res,next)=>{
     }
 });
 
+// get request is used for retrieving data 
 //:jobId --> parameter
 router.get('/details/:jobId',async(req,res,next)=>{
     try{
@@ -83,13 +86,39 @@ router.get('/details/:jobId',async(req,res,next)=>{
 
 // we use put when we want to update values / when we have updated as well as old values
 // we use patch when we only want to update a single value example:- like/unlike
-router.put('/edit/:jobId',(req,res)=>{
+router.put('/edit/:jobId',async(req,res,next)=>{
+    try{        
+        // fetching values (from req.body) and storing it in body (database)
+        const{
+            CompanyName,
+            Title,
+            Description,
+            LogoUrl,
+            Salary,
+            Location,
+            Duration,
+            LocationType
+        }=req.body;
+        console.log(CompanyName,Title,Description,LogoUrl,Salary,Location,Duration,LocationType);
+
+        // checking if all values are present
+        // here we are handling all the errors manually but we can also use a third party package
+        // YUP / JOI
+        if(!CompanyName || !Title || !Description || !LogoUrl || !Salary || !Location || !Duration || !LocationType){
+            return res.status(400).json({errorMessage:"Bad request"});
+        }
     try{
- 
+        const reqPayload=req.body;
+        await Job.updateOne(
+            {_id:jobId},
+            // ... is a spread operator which copies all the properties of reqPayload into new object
+            {$set:{...reqPayload,},}
+            );
+            res.json({message:"Job details updated successfully"});
     }
     catch(error)
     {
-
+        next(error);
     }
 });
 module.exports=router;
